8a594b13a5001fba832257b65db60204b6f70886
Vinithareddy09 <nallamadhivinitha04@gmail.com>
2025-02-03 20:07:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Quiz from "./Quiz"; // Import the Quiz component
+
+function App() {
+  return (
+    <div>
+      <h1>Quiz App</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
